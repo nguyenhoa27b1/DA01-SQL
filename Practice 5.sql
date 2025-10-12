d94b6cab6cf16e9123f1e0d7b5194e4d6789f4f4
nguyenhoa27b1 <nguyenhoa27b1@gmail.com>
2025-10-12 12:49:44 +0700

    Update Practice 5.sql

--- Practice 5.sql
+++ Practice 5.sql
@@ -65,4 +65,88 @@ group by a.product_name
 having sum(b.unit)>=100 
 
 ---Ex7 (page was not found)
+  
+-------MIDTERM TEST
+---Ex1
+select distinct replacement_cost from film
+order by replacement_cost 
+
+---Ex2
+select
+sum(case when replacement_cost between 9.99 and 19.99 then 1 else 0 end) as low,
+sum(case when replacement_cost between 20.00 and 24.99 then 1 else 0 end) as medium,
+sum(case when replacement_cost between 25.00 and 29.99 then 1 else 0 end) as high
+from film
+
+---Ex3
+select a.title, a.length, c.name
+from film as a
+inner join film_category as b
+on a.film_id=b.film_id
+inner join category as c
+on b.category_id=c.category_id
+where (c.name like '%Drama') or (c.name like '%Sports%')
+group by a.title, a.length, c.name
+order by a.length desc
+
+
+---Ex4
+select c.name,
+count(a.title)
+from film as a
+inner join film_category as b
+on a.film_id=b.film_id
+inner join category as c
+on b.category_id=c.category_id
+where (c.name like '%Drama') or (c.name like '%Sports%')
+group by c.name
+order by count(a.title) desc
+
+---Ex5
+select 
+concat(b.first_name,' ',b.last_name) as actor_name,
+count(a.film_id) as movies
+from film_actor as a
+inner join actor as b
+on a.actor_id=b.actor_id
+group by concat(b.first_name,' ',b.last_name)
+order by count(a.film_id) desc
+
+---Ex6
+select 
+sum(case when b.customer_id is null then 1 else 0 end) 
+from address as a
+left join customer as b
+on a.address_id=b.address_id
+
+---Ex7
+select a.city,
+sum(d.amount)
+from city as a
+join address as b
+on a.city_id=b.city_id
+join customer as c
+on b.address_id=c.address_id
+join payment as d
+on c.customer_id=d.customer_id
+group by a.city
+order by sum(d.amount) desc
+
+---Ex8
+select 
+concat(a.city,', ',e.country) as information,
+sum(d.amount)
+from city as a
+join address as b
+on a.city_id=b.city_id
+join customer as c
+on b.address_id=c.address_id
+join payment as d
+on c.customer_id=d.customer_id
+join country as e
+on a.country_id=e.country_id
+group by concat(a.city,', ',e.country)
+order by sum(d.amount) 
+
+
 
