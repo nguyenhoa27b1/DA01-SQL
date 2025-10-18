209b19caad76d7cd78a2f01407bc2d0e89288fc6
nguyenhoa27b1 <nguyenhoa27b1@gmail.com>
2025-10-18 12:00:14 +0700

    Update Practice 8.sql

--- Practice 8.sql
+++ Practice 8.sql
@@ -15,3 +15,26 @@ round(100*cast(sum(case when order_date=customer_pref_delivery_date then 1 else 
 from table1
 
 ---Ex2
+with 
+table1 as
+    (select player_id,
+    first_value(event_date) over(partition by player_id order by event_date) as first_day,
+    lead(event_date) over(partition by player_id order by event_date) as next_day
+    from Activity)
+select 
+round(cast(sum(case when (next_day-first_day)=1 then 1 else 0 end) as decimal)/cast(count(distinct player_id) as decimal),2) as fraction
+from table1
+
+---Ex3
+with table1 as 
+    (select *,
+    case 
+        when id%2=1 then lead(student) over(order by id)
+        when id%2=0 then lag(student) over(order by id)
+    end name_student
+    from Seat)
+select id,
+coalesce(name_student,student) as student
+from table1
+
+---Ex4
