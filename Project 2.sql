8880fb7647fa7a64374face736dfc3880eb8ce8b
nguyenhoa27b1 <nguyenhoa27b1@gmail.com>
2025-12-11 14:06:36 +0700

    Add revenue and order growth calculations to SQL

--- Project 2.sql
+++ Project 2.sql
@@ -70,3 +70,30 @@ from bigquery-public-data.thelook_ecommerce.order_items as a
 where created_at between '2019-01-01' and '2022-04-30'
 join bigquery-public-data.thelook_ecommerce.products as b
 on a.product_id=b.product_id
+
+5. 
+with table1 as
+(select 
+  extract (month from a.created_at) as month,
+  extract (year from a.created_at)  as year,
+c.category as product_category,
+sum(c.cost) as total_cost,
+sum(a.sale_price) as TPV,
+count(a.order_id) as TPO,
+from bigquery-public-data.thelook_ecommerce.order_items as a
+join bigquery-public-data.thelook_ecommerce.orders as b 
+on a.order_id=b.order_id
+join bigquery-public-data.thelook_ecommerce.products as c 
+on a.product_id=c.id
+group by 
+  extract (month from a.created_at),
+  extract (year from a.created_at),
+  c.category)
+select *,
+round(100*(TPV-lag(TPV) over(order by year,month))/lag(TPV) over(order by year,month),2)||'%' as revenue_growth,
+round(100*(TPO-lag(TPO) over(order by year,month))/lag(TPO) over(order by year,month),2)||'%' as 
+order_growth,
+TPV-total_cost as total_profit,
+(TPV-total_cost)/total_cost as profit_to_cost_ratio
+from table1
+order by year, month
